544db0f5e96754c781da733981b3f878f0e82533
NguyenNgocHien <nguyenngochien9464@gmail.com>
2024-09-26 17:21:28 +0700

    update them code lich su hoa don

--- BE/src/main/java/com/example/datn_f5_store/controller/AnhChiTietSanPhamController.java
+++ BE/src/main/java/com/example/datn_f5_store/controller/AnhChiTietSanPhamController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/anh-chi-tiet-san-pham")
 public class AnhChiTietSanPhamController {
---
+++ BE/src/main/java/com/example/datn_f5_store/repository/ILichSuHoaDonRepository.java
@@ -0,0 +1,15 @@
+package com.example.datn_f5_store.repository;
+
+import com.example.datn_f5_store.entity.LichSuHoaDonEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+
+public interface ILichSuHoaDonRepository extends JpaRepository<LichSuHoaDonEntity, Integer> {
+    @Query("SELECT l FROM LichSuHoaDonEntity l WHERE l.thoiGianThucHien BETWEEN :startDate AND :endDate")
+    Page<LichSuHoaDonEntity> findByThoiGianThucHienBetween(@Param("startDate") Date startDate, @Param("endDate") Date endDate, Pageable pageable);
+}
